e1ccb652b0a5205fdb32ec7a1125a4e022d4cd5a
Bong-oh Moon <bongohmoon@gmail.com>
2024-07-25 22:31:21 +0900

    [#1][init] spring initiaizer 생성

---
+++ src/main/java/com/tripmate/api/TripmateApiApplication.java
@@ -0,0 +1,13 @@
+package com.tripmate.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripmateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripmateApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tripmate/api/TestTripmateApiApplication.java
@@ -0,0 +1,11 @@
+package com.tripmate.api;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestTripmateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(TripmateApiApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
