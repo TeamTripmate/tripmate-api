e0d7799decb56b73f2b935dee6ee83a32ca37b4c
Bong-oh Moon <bongohmoon@gmail.com>
2024-09-17 22:19:11 +0900

    [#3] [style] swagger documentation(Controller, Request)

--- src/main/java/com/tripmate/api/controller/SpotController.java
+++ src/main/java/com/tripmate/api/controller/SpotController.java
@@ -4,9 +4,15 @@ import com.tripmate.api.dto.request.LocationBasedSpotSearchRequest;
 import com.tripmate.api.dto.response.LocationBasedSpotListResponse;
 import com.tripmate.api.dto.spot.LocationBasedSpotRecord;
 import com.tripmate.api.service.LocationBasedSpotSearchService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +21,23 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@Validated
 @RequestMapping("/api/v1/spots")
 public class SpotController {
 
     private final LocationBasedSpotSearchService locationBasedSpotSearchService;
 
+    @Operation(
+            summary = "내 근처 여행지 검색 API",
+            description = "위치(위도, 경도), 범위, 카테고리로 여행지 탐색"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful retrieval of spots")
+    })
     @GetMapping()
     public ResponseEntity<LocationBasedSpotListResponse> getSpots(
+            @Valid
+            @Parameter(description = "위치 및 필터 기준(카테고리)", required = true)
             @ModelAttribute LocationBasedSpotSearchRequest request
     ) {
         List<LocationBasedSpotRecord> spots = locationBasedSpotSearchService.searchLocationBasedSpots(request);
--- src/main/java/com/tripmate/api/dto/request/LocationBasedSpotSearchRequest.java
+++ src/main/java/com/tripmate/api/dto/request/LocationBasedSpotSearchRequest.java
@@ -1,12 +1,23 @@
 package com.tripmate.api.dto.request;
 
 import com.tripmate.api.domain.SpotCategory;
-import jakarta.validation.constraints.NotNull;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 
 public record LocationBasedSpotSearchRequest(
-        @NotNull String latitude,
-        @NotNull String longitude,
-        @NotNull String range,
+        @Schema(description = "위도", example = "128.922632")
+        @NotBlank(message = "위도(latitude) is required")
+        String latitude,
+
+        @Schema(description = "경도", example = "37.7563022")
+        @NotBlank(message = "경도(longitude) is required")
+        String longitude,
+
+        @Schema(description = "범위(반경)", example = "10000.0")
+        @NotBlank(message = "범위(range) is required")
+        String range,
+
+        @Schema(description = "여행지 카테고리(EXPERIENCE, CULTURE_AND_ART, etc.)", example = "EXPERIENCE")
         SpotCategory category
 ) {
 }
