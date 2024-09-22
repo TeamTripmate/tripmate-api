a22ad847b74c95b37c840bda99ca6cb38dec493d
juyongc <chunjy2147@gmail.com>
2024-09-22 14:10:51 +0900

    [refactor] 불필요한 import 제거

--- src/main/java/com/tripmate/api/dto/request/CollectCompanionRequest.java
+++ src/main/java/com/tripmate/api/dto/request/CollectCompanionRequest.java
@@ -2,8 +2,6 @@ package com.tripmate.api.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
-import lombok.Getter;
 
 public record CollectCompanionRequest(
 
--- src/main/java/com/tripmate/api/service/CompanionService.java
+++ src/main/java/com/tripmate/api/service/CompanionService.java
@@ -7,7 +7,6 @@ import com.tripmate.api.entity.CompanionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeMap;
 import org.springframework.stereotype.Service;
 
 @Slf4j
