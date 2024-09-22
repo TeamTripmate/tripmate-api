75b4b432f7665f71e113935f63c00997c5b6d5c9
Bong-oh Moon <bongohmoon@gmail.com>
2024-09-22 20:34:09 +0900

    [refactor] SwaggerConfig 사용 명시

--- src/main/java/com/tripmate/api/TripmateApiApplication.java
+++ src/main/java/com/tripmate/api/TripmateApiApplication.java
@@ -1,13 +1,14 @@
 package com.tripmate.api;
 
 import com.tripmate.api.config.AppConfig;
+import com.tripmate.api.config.SwaggerConfig;
 import com.tripmate.integration.tourapi.config.TourApiConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({ AppConfig.class, TourApiConfig.class })
+@Import({ AppConfig.class, SwaggerConfig.class, TourApiConfig.class })
 public class TripmateApiApplication {
 
     public static void main(String[] args) {
