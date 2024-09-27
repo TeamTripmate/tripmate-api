b8a0ceecf8dc60fb1aac47c7f4fd9ed5867cfd82
juyongc <chunjy2147@gmail.com>
2024-09-27 10:13:18 +0900

    [update] 마이페이지 - 유저 정보 확인 API 비즈니스 로직 개발

--- src/main/java/com/tripmate/api/login/LoginController.java
+++ src/main/java/com/tripmate/api/login/LoginController.java
@@ -7,11 +7,7 @@ import com.tripmate.api.dto.response.MypageUserInfoResponse;
 import com.tripmate.api.dto.response.TripmateApiResponse;
 import com.tripmate.api.dto.response.TripmatePersonalizedTestResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -79,16 +75,13 @@ public class LoginController {
         summary = "마이페이지 - 유저 정보 확인 API"
     )
     @GetMapping("user/{userId}")
-    @SecurityRequirement(name = "bearerAuth")
+//    @SecurityRequirement(name = "bearerAuth")
     public ResponseEntity<TripmateApiResponse<MypageUserInfoResponse>> getUserInfo(@PathVariable("userId") Long userId) {
 
-        kakaoLoginService.getMypageUserInfo(userId);
-
-        String[] array = {"맛집탐험형", "액티비티형", "쇼핑형"};
-        List<String> keywordList = new ArrayList<>(Arrays.asList(array));
+        MypageUserInfoResponse mypageUserInfo = kakaoLoginService.getMypageUserInfo(userId);
 
         return ResponseEntity.ok(TripmateApiResponse.success(
-            new MypageUserInfoResponse(keywordList,"PENGUIN","여행가 아기꿀벌","카닉","이미지썸네일URL", "이미지URL")
+            mypageUserInfo
         ));
     }
 
