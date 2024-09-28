1272c4e1fb334b543fa4a1d3d48aa1053d0a84bf
juyongc <chunjy2147@gmail.com>
2024-09-28 14:23:45 +0900

    [fix] 마이페이지 - 유저 정보 확인 API 비즈니스 로직 수정
    - 개인화 비즈니스 로직 미개발로 인한 left join으로 조인 방식 수정

--- src/main/java/com/tripmate/api/entity/UserRepository.java
+++ src/main/java/com/tripmate/api/entity/UserRepository.java
@@ -8,11 +8,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
+    // TODO : 개인화 개발 완료되면 LEFT JOIN -> JOIN 변경하기
     @Query(
         "select new com.tripmate.api.dto.response.MypageUserInfoResponse(tse.keyword1, tse.keyword2, tse.keyword3, "
             + "ue.characterType, tse.styleName, ue.nickname, ue.thumbnailImage, ue.profileImage) "
             + "from UserEntity ue "
-            + "JOIN TripStyleEntity tse ON tse.id = ue.tripStyleId "
+            + "LEFT JOIN TripStyleEntity tse ON tse.id = ue.tripStyleId "
             + "WHERE ue.kakaoId = :userId"
     )
     MypageUserInfoResponse joinUserEntityAndTripStyleEntity(Long userId);
