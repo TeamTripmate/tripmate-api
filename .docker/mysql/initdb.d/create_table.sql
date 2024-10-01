c8fc0d48a54f34aa9aa31f9135ae8abec44da123
Bong-oh Moon <bongohmoon@gmail.com>
2024-10-02 01:48:15 +0900

    [feat] docker mysql DB 테이블 초기화 스크립트 trip_style 수정

--- .docker/mysql/initdb.d/create_table.sql
+++ .docker/mysql/initdb.d/create_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE trip_style_entity
 (
     id         BIGINT AUTO_INCREMENT PRIMARY KEY,
-    style_name VARCHAR(255),
-    keyword1   VARCHAR(255),
-    keyword2   VARCHAR(255),
-    keyword3   VARCHAR(255)
+    style_name VARCHAR(127),
+    keyword1   VARCHAR(15),
+    keyword2   VARCHAR(15),
+    keyword3   VARCHAR(15)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_unicode_ci;
